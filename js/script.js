1348afd2eb16e1abf5cfecb2713f2dd7392cd1f2
Dzuev76 <Dzuev76@gmail.com>
2018-04-18 22:03:47 +0600

    change

---
+++ js/script.js
@@ -0,0 +1,102 @@
+var myCircle = Circles.create({
+  id:                  'circles-1',
+  radius:              60,
+  value:               95,
+  maxValue:            100,
+  width:               10,
+  text:                function(value){return value + '%';},
+  colors:              ['#52edc7', '#fff'],
+  duration:            9000,
+  wrpClass:            'circles-wrp',
+  textClass:           'circles-text',
+  valueStrokeClass:    'circles-valueStroke',
+  maxValueStrokeClass: 'circles-maxValueStroke',
+  styleWrapper:        true,
+  styleText:           true
+});
+
+var myCircle = Circles.create({
+  id:                  'circles-2',
+  radius:              60,
+  value:               95,
+  maxValue:            100,
+  width:               10,
+  text:                function(value){return value + '%';},
+  colors:              ['#52edc7', '#fff'],
+  duration:            9000,
+  wrpClass:            'circles-wrp',
+  textClass:           'circles-text',
+  valueStrokeClass:    'circles-valueStroke',
+  maxValueStrokeClass: 'circles-maxValueStroke',
+  styleWrapper:        true,
+  styleText:           true
+});
+
+var myCircle = Circles.create({
+  id:                  'circles-3',
+  radius:              60,
+  value:               45,
+  maxValue:            100,
+  width:               10,
+  text:                function(value){return value + '%';},
+  colors:              ['#52edc7', '#fff'],
+  duration:            9000,
+  wrpClass:            'circles-wrp',
+  textClass:           'circles-text',
+  valueStrokeClass:    'circles-valueStroke',
+  maxValueStrokeClass: 'circles-maxValueStroke',
+  styleWrapper:        true,
+  styleText:           true
+});
+
+var myCircle = Circles.create({
+  id:                  'circles-4',
+  radius:              60,
+  value:               95,
+  maxValue:            100,
+  width:               10,
+  text:                function(value){return value + '%';},
+  colors:              ['#52edc7', '#fff'],
+  duration:            9000,
+  wrpClass:            'circles-wrp',
+  textClass:           'circles-text',
+  valueStrokeClass:    'circles-valueStroke',
+  maxValueStrokeClass: 'circles-maxValueStroke',
+  styleWrapper:        true,
+  styleText:           true
+});
+
+var myCircle = Circles.create({
+  id:                  'circles-5',
+  radius:              60,
+  value:               70,
+  maxValue:            100,
+  width:               10,
+  text:                function(value){return value + '%';},
+  colors:              ['#52edc7', '#fff'],
+  duration:            9000,
+  wrpClass:            'circles-wrp',
+  textClass:           'circles-text',
+  valueStrokeClass:    'circles-valueStroke',
+  maxValueStrokeClass: 'circles-maxValueStroke',
+  styleWrapper:        true,
+  styleText:           true
+});
+
+var myCircle = Circles.create({
+  id:                  'circles-6',
+  radius:              60,
+  value:               30,
+  maxValue:            100,
+  width:               10,
+  text:                function(value){return value + '%';},
+  colors:              ['#52edc7', '#fff'],
+  duration:            9000,
+  wrpClass:            'circles-wrp',
+  textClass:           'circles-text',
+  valueStrokeClass:    'circles-valueStroke',
+  maxValueStrokeClass: 'circles-maxValueStroke',
+  styleWrapper:        true,
+  styleText:           true
+});
+
